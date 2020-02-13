f49e408ef6164986c36485c08aa3aa6bf1b1887c
Cristal Alatorre Perez <cristalcodes@gmail.com>
2020-02-13 18:58:15 -0500

    Adds fetch request to backend to acquire all pin locations.

--- src/map.js
+++ src/map.js
@@ -1,59 +1,54 @@
+
 //Initiates Map
 function initMap(){
-  var options = {
-    zoom: 1,
-    center: {lat: 42.3601, lng: -71.0589}
-  }
+    var options = {
+      zoom: 1,
+      center: {lat: 42.3601, lng: -71.0589}
+    }
   //Creates a new map
   var map = new google.maps.Map(document.getElementById('map'),options);
 
-  //Listen for click on map & //add marker
-  google.maps.event.addListener(map, 'click', function(event){
-     addMarker({coords:event.latLng});
-  });
+  //fetches information from backend
+  fetch("http://localhost:3000")
+  .then(response => response.json())
+  .then(jsonData => {
+  //iterates through each location object & sets variables
+    jsonData.forEach((location) =>  {
+      let pinId = location['id'];
+      let pinLabel = location['label'];
+      let pinLatitude = location['latitude'];
+      let pinlongitude = location['longitude'];
+  //creates a pin using above variables
+      pinInfo = {
+        id: pinId,
+        label: pinLabel,
+        coords: {
+          lat: pinLatitude,
+          lng: pinlongitude
+        }
+      }
+  //calls the addMarker function and passes the pinInfo object as a param
+      addMarker(pinInfo);
+    })
+  })
 
 
-  //Array of Markers
-  var markers =[
-    {
-      coords: {lat: 42.4668, lng: -70.9495},
-       content: '<h1>Lynn MA</h1>'
-    },
-    {
-      coords: {lat: 34.6868 , lng: -118.1542}
-    },
-    {
-      coords: {lat: 35.8826 , lng: -80.0820}
+//adds pins to the map
+  function addMarker(pin){
+    var marker = new google.maps.Marker({
+      map: map,
+      position: pin.coords,
+    })
+
+    if (pin.label){
+      var infoWindow = new google.maps.InfoWindow({
+        content: `<h1>${pin.label}</h1>`
+      });
+
+      marker.addListener('click', function(){
+        infoWindow.open(map, marker);
+      });
     }
-
-  ];
-
-
-  //Loop through Markers
-  for (var i=0; i< markers.length; i++){
-    addMarker(markers[i]);
   }
-
-
-
-
-   //Add Marker Function
-     function addMarker(props){
-       var marker = new google.maps.Marker({
-         map: map,
-         position: props.coords,
-       })
-
-       if (props.content){
-         var infoWindow = new google.maps.InfoWindow({
-           content: props.content
-         });
-
-         marker.addListener('click', function(){
-           infoWindow.open(map, marker);
-         });
-       }
-     }
-
 
 }
